7023022ed0a69c0864ecd1b868d11923f79f7f82
Success Enehikhuere <enehikhueresuccess@gmail.com>
2024-11-20 12:06:50 +0100

    "set up routes"

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { BrowserRouter, Route, Routes, } from "react-router-dom"
 import Home from "./Home"
+import Create from "./Create"
+import Update from "./Update"
+
 function App() {
- 
   return (
-    <main>
-      <Home />
-    </main>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/create" element={<Create />}></Route>
+        <Route path="/edit/:id" element={<Update />}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
