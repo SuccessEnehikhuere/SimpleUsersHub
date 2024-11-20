3b0a3651e3945e4aae988d8355fcaec622789f09
Success Enehikhuere <enehikhueresuccess@gmail.com>
2024-11-20 12:07:39 +0100

    "create the table for users"

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 function Home() {
+  const users = useSelector((state) => state.users)
+
   return (
-    <div className='container'>
+    <div className="container">
       <h2>Users List</h2>
-      <button className='btn btn-success my-3'>Create</button>
-      <table className='table'>
+      <Link to='/create' className="btn btn-primary my-3">Create +</Link>
+      <table className="table">
         <thead>
           <tr>
             <th>ID</th>
@@ -14,6 +18,21 @@ function Home() {
             <th>Action</th>
           </tr>
         </thead>
+        <tbody>
+          {users.map((user, index) => {
+            return (
+              <tr key={index}>
+                <td>{user.id}</td>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>
+                  <Link to={`/edit/${user?.id}`} className="btn btn-sm btn-secondary">Edit</Link>
+                  <button className="btn btn-sm btn-danger ms-2">delete</button>
+                </td>
+              </tr>
+            )
+          })}
+        </tbody>
       </table>
     </div>
   )
